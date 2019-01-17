2d28c0a7406deac5d5c27cd5cc6281b22258bc39
luqman <luqmansen@gmail.com>
2019-01-17 23:23:37 +0700

    add limit to hour in timepicker 7am - 5pm

--- resources/views/reservation/booking-form.blade.php
+++ resources/views/reservation/booking-form.blade.php
@@ -108,7 +108,9 @@
         'timeFormat': 'g:ia',
         'disableTextInput' : true,
         'disableTouchKeyboard' : true,
-        'disableTimeRanges' : [['12:00am', '7:00am'], ['5:30pm', '11:30pm']]
+        'minTime' : '7:00am',
+        'maxTime' : '5:00pm',
+        'disableTimeRanges' : [['12:00 am', '7:00am'], ['5:30pm', '11:30pm']]
         });
 
         var timeOnlyExampleEl = document.getElementById('timeOnlyExample');
